62c9ac2cc275a1e41e8efe87d347b7ef5b0d1bab
tinyladi <ladislas@weareleka.com>
2014-05-22 14:08:56 +0200

    various modification to investigate...

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
-$(function(){
-
-	var url = window.location.pathname;
-	var activePage = url;
-
-	$('.masthead-nav li a').each(function(){
-		var href = $(this).attr('href');
-		if (activePage === href) {
-			$('#home').removeClass('active');
-			$(this).parent().addClass('active');
-		}
-	});
-});
+// $(function(){
+//
+// 	var url = window.location.pathname;
+// 	var activePage = url;
+//
+// 	$('.navbar-nav li a').each(function(){
+// 		var href = $(this).attr('href');
+// 		if (activePage === href) {
+// 			$('#home').removeClass('active');
+// 			$(this).parent().addClass('active');
+// 		}
+// 	});
+// });
