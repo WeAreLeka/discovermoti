1f44a75855aeb892585a85f9ab9b885966665b25
tinyladi <ladislas@weareleka.com>
2014-05-26 16:27:14 +0200

    sarting css

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,25 @@
-<script>
-	jQuery(document).ready(function () {
+jQuery(document).ready(function () {
 
-		var vidUrl = $("#splash > source").attr('src');
+	var vidUrl = $("#splash > source").attr('src');
 
-		// make sure the video is avalaible and if not, hide the video element to avoid black screen
-		$.get(vidUrl).fail(function() {
-			$("#splash").hide();
-		});
-
-		// get video width and height
-		$("#splash").on("loadedmetadata", function () {
-			$("#splash").css('visibility', 'visible');
-			centerVid();
-		});
-
-		//functions declarations
-		function centerVid() {
-			$("#splash").css('right', (($(window).width()/2) - ($("#splash").width()/2)));
-		}
-
-		// update the video size and position on resize.
-		$(window).resize(function() {
-			centerVid();
-		});
+	// make sure the video is avalaible and if not, hide the video element to avoid black screen
+	$.get(vidUrl).fail(function() {
+		$("#splash").hide();
 	});
-</script>
+
+	// get video width and height
+	$("#splash").on("loadedmetadata", function () {
+		$("#splash").css('visibility', 'visible');
+		centerVid();
+	});
+
+	//functions declarations
+	function centerVid() {
+		$("#splash").css('right', (($(window).width()/2) - ($("#splash").width()/2)));
+	}
+
+	// update the video size and position on resize.
+	$(window).resize(function() {
+		centerVid();
+	});
+});
