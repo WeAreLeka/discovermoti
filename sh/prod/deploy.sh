53231467e8a1486a0165dd6422a306c3cd53e510
gsellator <adrien@gsell.me>
2015-02-06 17:49:36 +0100

    Node js server

---
+++ sh/prod/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# set user variables
+PATH='/Users/adriengsell/.nvm/v0.10.36/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin:/opt/X11/bin:/usr/local/git/bin'
+SOURCEFOLDER='Sources'
+USR='9h00'
+VERSION='prod'
+SERVER='delirium.cloudapp.net'
+
+# Deploy sources
+cd ~/$SOURCEFOLDER/$USR/$USR
+grunt $VERSION
+
+# Run the app
+APPSCRIPT='cd /home/'$USR-$VERSION'/'$USR'/ ;\
+npm install --production ;\
+export NODE_ENV='$VERSION' ;\
+forever stop app.js ;\
+forever start app.js ;\
+sudo cp /home/'$USR-$VERSION'/'$USR'/sh/'$VERSION'/robots.txt /home/'$USR-$VERSION'/'$USR'/client/ ;\
+sudo cp /home/'$USR-$VERSION'/'$USR'/sh/'$VERSION'/nginx.conf /etc/nginx/sites-available/'$USR-$VERSION' ;\
+sudo ln -s /etc/nginx/sites-available/'$USR-$VERSION' /etc/nginx/sites-enabled/ ;\
+sudo service nginx reload'
+ssh -t $USR-$VERSION@$SERVER "$APPSCRIPT"
